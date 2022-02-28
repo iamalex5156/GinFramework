9ad39c8406e05c6e00df172df3536c34c7b6d0b6
Alex Xu <75003875+iamalex5156@users.noreply.github.com>
2022-02-28 14:49:11 +0800

    middleware

---
+++ gee/gee.go
@@ -0,0 +1,73 @@
+package gee
+
+import (
+	"log"
+	"net/http"
+)
+
+type HandlerFunc func(*Context)
+
+type (
+	RouterGroup struct {
+		prefix      string
+		middlewares []HandlerFunc
+		parent      *RouterGroup
+		engine      *Engine
+	}
+
+	Engine struct {
+		*RouterGroup
+		router *router
+		groups  []*RouterGroup
+	}
+)
+
+func (engine *Engine) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	//key:=request.Method+"-"+request.URL.Path
+	//if handler,ok:=engine.router[key];ok{
+	//	handler(writer,request)
+	//}else{
+	//	fmt.Fprintf(writer,"404 NOT FOUND:%s\n",request.URL)
+	//}
+	//
+	c := newContext(writer, request)
+	engine.router.handle(c)
+
+}
+
+func (group *RouterGroup)Group(prefix string)*RouterGroup{
+	engine:=group.engine
+	newGroup:=&RouterGroup{
+		prefix: group.prefix+prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups=append(engine.groups,newGroup)
+	return newGroup
+}
+
+
+func New() *Engine {
+	engine:=&Engine{router: newRouter()}
+	engine.RouterGroup=&RouterGroup{engine: engine}
+	engine.groups=[]*RouterGroup{engine.RouterGroup}
+	return engine
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern:=group.prefix+comp
+	log.Printf("Route %4s-%s",method,pattern)
+	group.engine.router.addRoute(method,pattern,handler)
+}
+
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET",pattern,handler)
+}
+
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST",pattern,handler)
+}
+
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
